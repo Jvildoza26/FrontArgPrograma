caad5cb04aaaa5d4f4cf6ed7be78da5de6c0075b
Jvildoza26 <jonathanvildoza26@gmail.com>
2022-08-30 11:13:28 -0300

    inicio

--- FrontArgPrograma/src/app/app.module.ts
+++ FrontArgPrograma/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BanerComponent } from './componentes/baner/baner.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BanerComponent
+    BanerComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,

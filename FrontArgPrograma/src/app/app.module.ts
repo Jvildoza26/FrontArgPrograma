1d6f939c982674a8678fd6ee9e8da5ad1a8500c8
Jvildoza26 <jonathanvildoza26@gmail.com>
2022-08-30 11:22:06 -0300

    social

--- FrontArgPrograma/src/app/app.module.ts
+++ FrontArgPrograma/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BanerComponent } from './componentes/baner/baner.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { LogoComponent } from './componentes/logo/logo.component';
+import { SocialComponent } from './componentes/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BanerComponent,
     InicioComponent,
-    LogoComponent
+    LogoComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
